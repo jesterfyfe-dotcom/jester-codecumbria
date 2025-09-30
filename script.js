17a5bb5c755a0a65eef483f86461c0e50bc320a5
jesterfyfe-dotcom <jesterfyfe@gmail.com>
2025-09-30 20:18:56 +0100

    upgrades-added

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
 const hedgehogImg = document.getElementById("hedgehog-img");
 const wormImg = document.getElementById("worm-img");
 const wormEatingDisplay = document.getElementById("worm-eating");
+const wormCatchingUpgrade = document.getElementById("worm-catching-upgrade");
 
 let hedgehogImgNum = 1;
 let wormsEaten = 0;
+let wormCatchingLevel = 0;
+const wormCatchingUpgradePrice = [25,100,250,500]
 
 hedgehogImg.addEventListener("click", function(){
 if(hedgehogImgNum===1){
@@ -25,7 +28,23 @@ function updateWormDisplay(){
     wormEatingDisplay.textContent = `Worms eaten: ${wormsEaten}`
 }
 
+function updateWormCatching(){
+    wormCatchingUpgrade.textContent = `Worm Catching ${wormCatchingLevel+2} - [${wormCatchingUpgradePrice[wormCatchingLevel]} Worms]`
+}
+
 wormImg.addEventListener("click", function(){
-    wormsEaten ++;
+    wormsEaten += (wormCatchingLevel+1);
     updateWormDisplay();
+})
+
+wormCatchingUpgrade.addEventListener("click", function(){
+if(wormsEaten>=wormCatchingUpgradePrice[wormCatchingLevel]){
+    wormsEaten -= wormCatchingUpgradePrice[wormCatchingLevel]
+    wormCatchingLevel++;
+    updateWormDisplay();
+    updateWormCatching();
+}
+if(wormCatchingLevel === wormCatchingUpgradePrice.length){
+    wormCatchingUpgrade.classList.add("hidden");
+}
 })

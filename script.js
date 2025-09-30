c7d2165583b0474cf6951b50bacb9022ccf905fa
jesterfyfe-dotcom <jesterfyfe@gmail.com>
2025-09-30 18:51:46 +0100

    important changes maybe

--- script.js
+++ script.js
@@ -1,5 +1,25 @@
 const headgehogLeaf = document.getElementById("hedgehog1")
+const headgehogStare = document.getElementById("hedgehog2")
+const headgehogYay = document.getElementById("hedgehog3")
+const headgehogFlowers = document.getElementById("hedgehog4")
+const headgehogFrens = document.getElementById("hedgehog5")
 
-headgehogLeaf.addEventListener("click", function(){
-    console.log("success")
-})
+function changeHedgehogs(num){
+headgehogLeaf.classList.add("hidden")
+headgehogStare.classList.add("hidden")
+headgehogYay.classList.add("hidden")
+headgehogFlowers.classList.add("hidden")
+headgehogFrens.classList.add("hidden")
+
+if(num === 1) headgehogLeaf.classList.remove("hidden");
+if(num === 2) headgehogStare.classList.remove("hidden");
+if(num === 3) headgehogYay.classList.remove("hidden");
+if(num === 4) headgehogFlowers.classList.remove("hidden");
+if(num === 5) headgehogFrens.classList.remove("hidden");
+}
+
+headgehogLeaf.addEventListener("click", changeHedgehogs(1));
+headgehogStare.addEventListener("click", changeHedgehogs(2));
+headgehogYay.addEventListener("click", changeHedgehogs(3));
+headgehogFlowers.addEventListener("click", changeHedgehogs(4));
+// headgehogFrens.addEventListener("click", changeHedgehogs(5));
